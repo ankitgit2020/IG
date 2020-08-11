a2969527bf18ae9c2f30cde9d6fd1fa7770851c8
kavya <kavya@192.168.1.24>
2020-08-11 16:01:42 +0530

    Gateway with basic authentication

--- cloud-gateway-service/src/main/java/com/poc/cloudgateway/CloudGatewayServiceApplication.java
+++ cloud-gateway-service/src/main/java/com/poc/cloudgateway/CloudGatewayServiceApplication.java
@@ -1,12 +1,8 @@
 package com.poc.cloudgateway;
 
-import java.security.Principal;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,22 +12,6 @@ public class CloudGatewayServiceApplication {
 		SpringApplication.run(CloudGatewayServiceApplication.class, args);
 	}
 
-//  @Bean	
-//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder, 
-//			TokenRelayGatewayFilterFactory tokenRelay) {
-//	
-//		return builder.routes()
-//				.route("servlet-app", r -> r.path("/movie-info-service", "/rating-data-service")
-//				.filters(f -> f.filter(tokenRelay.apply()))
-//				.uri("http://localhost:8000"))
-//				.build();
-//
-//	}
 	
-  
-  @GetMapping("/")
-	public String message(Principal principal) {
-		return "Hi Gateway app "+principal.getName()+" welcome to SpringCloudOauth2ExampleApplication";
-	}
 	
 }
--- cloud-gateway-service/src/main/java/com/poc/cloudgateway/config/SecurityConfig.java
+++ cloud-gateway-service/src/main/java/com/poc/cloudgateway/config/SecurityConfig.java
@@ -1,44 +1,33 @@
 package com.poc.cloudgateway.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
-
-  @Bean
-  public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
-    http.authorizeExchange()
-        .pathMatchers("/actuator/**")
-        .hasRole("ACTUATOR")
-        .and()
-        .httpBasic()
-        .and()
-        .authorizeExchange()
-        .anyExchange()
-        .authenticated()
-        .and()
-        .oauth2Client();
-    return http.build();
-  }
-  //@Bean
-  //ClientRegistrationRepository clientRegistrationRepository;
-
-	@Configuration
-	static class AuthorizedClient {
-		@Bean
-		public OAuth2AuthorizedClientRepository authorizedClientRepository() {
-			return new HttpSessionOAuth2AuthorizedClientRepository();
-		}
-	}
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {//extends WebSecurityConfigurerAdapter {
+ 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//         .anyRequest().authenticated()
+//         .and()
+//         .oauth2Login();
+//    }
+    
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated()
+//          .and()
+//          .oauth2Login()
+//          .clientRegistrationRepository(clientRegistrationRepository())
+//          .authorizedClientService(authorizedClientService());
+//    }
+     
+//    @Bean
+//    public OAuth2AuthorizedClientService authorizedClientService() {
+//     
+//        return new InMemoryOAuth2AuthorizedClientService(
+//          clientRegistrationRepository());
+//    }
 }

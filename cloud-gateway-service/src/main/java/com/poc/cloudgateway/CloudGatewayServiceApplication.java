f38640b60be75cd6f639de71ea8fe25f46fb2237
kavya <kavya@192.168.1.24>
2020-08-12 15:57:19 +0530

    Cloud gateway with zuul

--- cloud-gateway-service/src/main/java/com/poc/cloudgateway/CloudGatewayServiceApplication.java
+++ cloud-gateway-service/src/main/java/com/poc/cloudgateway/CloudGatewayServiceApplication.java
@@ -3,15 +3,15 @@ package com.poc.cloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class CloudGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayServiceApplication.class, args);
 	}
-
-	
 	
 }
